0a95de872a30111530ac5228e3b660bf6f254408
Leonardo Lima <dev.leonardolima@gmail.com>
2022-05-02 23:39:24 -0400

    config timeout on MessageError

--- src/components/MessageError/index.tsx
+++ src/components/MessageError/index.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from "../../contexts/auth";
 import * as S from "./styles";
 
 type Props = {
@@ -5,9 +6,14 @@ type Props = {
 };
 
 export function MessageError({ message }: Props) {
+  const { updateMessage } = useAuth();
+  setTimeout(() => {
+    updateMessage("");
+  }, 5000);
+
   return (
     <S.MessageError>
-      <S.Span>{message}</S.Span>
+      <S.Span>{message}</S.Span>;
     </S.MessageError>
   );
 }
