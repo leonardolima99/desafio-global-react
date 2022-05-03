1385a167486c9614a0d580d76ff8a65a4964df86
Leonardo Lima <dev.leonardolima@gmail.com>
2022-05-03 07:35:16 -0400

    refactor component Message

---
+++ src/components/Message/index.tsx
@@ -0,0 +1,50 @@
+import * as S from "./styles";
+
+type Props = {
+  message: Array<string>;
+  type: "error" | "success" | "info" | undefined;
+};
+
+export function Message({ message, type }: Props) {
+  switch (type) {
+    case "error":
+      return (
+        <S.MessageWrap>
+          {message.map((text) => (
+            <S.MessageError key={text}>
+              <S.Span>{text}</S.Span>;
+            </S.MessageError>
+          ))}
+        </S.MessageWrap>
+      );
+
+    case "success":
+      return (
+        <S.MessageWrap>
+          {message.map((text) => (
+            <S.MessageSuccess key={text}>
+              <S.Span>{text}</S.Span>;
+            </S.MessageSuccess>
+          ))}
+        </S.MessageWrap>
+      );
+
+    case "info":
+      return (
+        <S.MessageWrap>
+          {message.map((text) => (
+            <S.MessageInfo key={text}>
+              <S.Span>{text}</S.Span>;
+            </S.MessageInfo>
+          ))}
+        </S.MessageWrap>
+      );
+    default:
+      return null;
+    /* return (
+        <S.MessageWarning>
+          <S.Span>Algo inesperado aconteceu.</S.Span>;
+        </S.MessageWarning>
+      ); */
+  }
+}
---
+++ src/components/Message/styles.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const MessageWrap = styled.div``;
+
+export const MessageError = styled.div`
+  width: auto;
+  height: 48px;
+  margin: 16px;
+  background-color: rgba(255, 38, 38, 0.9);
+  justify-content: center;
+  align-content: center;
+`;
+
+export const MessageSuccess = styled.div`
+  width: auto;
+  height: 48px;
+  margin: 16px;
+  background-color: rgba(37, 240, 43, 0.8);
+  justify-content: center;
+  align-content: center;
+`;
+
+export const MessageInfo = styled.div`
+  width: auto;
+  height: 48px;
+  margin: 16px;
+  background-color: rgba(40, 185, 255, 0.9);
+  justify-content: center;
+  align-content: center;
+`;
+
+export const MessageWarning = styled.div`
+  width: auto;
+  height: 48px;
+  margin: 16px;
+  background-color: rgba(197, 197, 35, 0.9);
+  justify-content: center;
+  align-content: center;
+`;
+
+export const Span = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 48px;
+  color: #f5f5f5;
+  padding: 16px;
+`;
